43eb22d850a7b250ae591e7b5d5499095c06a745
acmeid <406652727@qq.com>
2015-12-15 11:18:58 +0800

    update

--- src/views/components.vue
+++ src/views/components.vue
@@ -1,38 +1,23 @@
 <template>
-  <div @click="ons(2)">
+  <div>
     compontents demo2
   </div>
-  <ul>
-    <li v-for="item in array">{{item}}</li>
-  </ul>
 </template>
 
 <script>
-import resApi form "../"
 export default {
   data () {
     return {
-      array: []
     }
   },
   compiled(){
-    this.get()
   },
 
   methods: {
-    ons(item){
-      this.array = [3,4,5]
-    },
-    get(){
-      let data = [1,2]
-      this.array = data
-    }
-    post(){
-      resApi.getCode.save()
-    }
   }
 }
 </script>
+
 <style lang="less">
 
 </style>
